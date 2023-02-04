8eabbb7a2d950b9f2cdc838279bf9f4c56a627ec
Antti Lehtonen <antti.lehtonen@wunderdog.fi>
2023-02-04 18:37:36 +0200

    TODO table started.

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { token } from "../stores";
+  import { stringify } from "postcss";
+import { token } from "../stores";
   import { TODO_ENDPOINT } from "../utils/constants";
   import Button from "./Button.svelte";
 
@@ -8,18 +9,21 @@
     currentToken = value;
   });
 
-  let todos;
+  /**
+   * @type {object[]}
+   */
+  let todos = [];
   let error = "";
   async function getTodos() {
     try {
       error = "";
       const headers = {
-        'Authorization': `Bearer ${currentToken}`
+        Authorization: `Bearer ${currentToken}`,
       };
       const response = await fetch(`${TODO_ENDPOINT}/get`, {
         headers,
       });
-      const data = await response.json();
+      todos = await response.json();
     } catch (e) {
       error = e;
     }
@@ -28,11 +32,27 @@
 
 <div class="container mx-auto bg-slate-300 shadow border p-8">
   <Button on:click={getTodos} label="Fetch TODOS" />
-  <h1>TODOS</h1>
-  {#if todos}
-    <p>You fetched {todos}</p>
-  {/if}
+  <h2>TODOS {JSON.stringify(todos)}</h2>
   {#if error}
     <p>Error occured: {error}</p>
   {/if}
+  {#if todos.length}
+    <p>Fetched {todos.length} items</p>
+  {/if}
+  <table>
+    <thead>
+      <th>Id</th>
+      <th>Name</th>
+      <th></th>
+    </thead>
+    <tbody>
+      {#each todos as { todoId, name }, i}
+      <tr>
+        <td>{todoId}</td>
+        <td>{i + 1}: {name}</td>
+        <td></td>
+      </tr>
+      {/each}
+    </tbody>
+  </table>
 </div>
